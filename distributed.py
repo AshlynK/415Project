ecc2b729724892f9f27e40eebff3281fdfc890b6
greg-pedersen <71673441+greg-pedersen@users.noreply.github.com>
2020-12-10 15:24:58 -0800

    Update distributed.py
    Updating with the final product/code for our distributed algorithm

--- distributed.py
+++ distributed.py
@@ -1,20 +1,17 @@
 import pyspark
 from pyspark.sql.session import SparkSession
-
 from pyspark.sql import SQLContext
 from pyspark import SparkFiles
+from pyspark.sql.functions import col
+import pyspark.sql.functions as F
 from pyspark.sql.functions import concat, col, lit
 from pyspark.sql.functions import monotonically_increasing_id, row_number
 from pyspark.sql import Window
-from pyspark.sql.functions import col
-import pyspark.sql.functions as F
 
 #reading in/adjusting the 3 data frames from github
 
-#Here is how I read in the files, its a bit tedious but not too bad. Hopefully this works for you two as well. 
-
 #Airports
-url = "https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat"
+url = "https://raw.githubusercontent.com/AshlynK/415Project/main/airports.csv"
 sc.addFile(url)
 sqlContext = SQLContext(sc)
 
@@ -45,10 +42,6 @@ df3 = sqlContext3.read.csv(SparkFiles.get("airlines.dat"), header=False, inferSc
 new_col_names3 = "Airline ID", "Airline Name", "Alias", "IATA", "ICAO", "Callsign", "Country", "Active (Y/N)"
 df3 = df3.toDF(*new_col_names3)
 
-
-############################################################Find Flight Algorithm#################################################################################
-from pyspark.sql.functions import col
-import pyspark.sql.functions as F
 
 def find_flight():
     source = input("Enter Your Source City: ")
@@ -95,14 +88,10 @@ def find_flight():
     stops = list(flights2.select("Stops").toPandas()["Stops"])
     for h, i, j, k in zip(source, dest, airline_names, stops):
         print("Flight from", h, "to", i, "on", j, "with", k, "stops")
-    
+        
+        
 
-    
-find_flight()
-###################################################################find_popular_airports####################################################################################
-
-
-def find_popular_airports():
+def find_popular_airport():
     amount = input("How many airports would you like to see ranked: ")
     df3 = df2.groupBy('Source Airport').count()
     a = df3.sort(col('count').desc())
@@ -123,10 +112,29 @@ def find_popular_airports():
     flights = flights[0:int(amount)]
     print("Most Popular Airports by Flight: ")
     for i, j in zip(cities, flights):
-        print(i,":",j)
-    ##############################################################################################################################################################
-#prints info on airports in given city
-def print_info():
+        print(i,":",j)    
+        
+        
+        
+def airports_by_country():
+    area = input("Please enter a country: ")
+    
+    country_of_area = df.filter(col("Country") == area)
+    data_output = list(country_of_area.select('Name').toPandas()['Name'])
+    data_output1 = list(country_of_area.select('City').toPandas()['City'])
+    
+    for i, j in zip(data_output, data_output1):
+        print(i, ':', j)
+     
+    
+def country_rankings():
+    ranking = input("How many countries would you like to see ranked: ")
+    counts = df.groupBy('Country').count()
+    counts2 = counts.sort(col('count').desc())
+    counts2.show(int(ranking))
+    
+    
+def airport_info():
     city= input("Enter City to output airport(s) info: ")
     
     airports = df.filter(col("City") == city)
@@ -137,45 +145,49 @@ def print_info():
     lat = list(airports.select('Lat').toPandas()['Lat'])
     long = list(airports.select('Long').toPandas()['Long'])
     
-    for x in range(len(airports)):
-        print("\n", names[x])
-        print("\nCity:", airports[x])
-        print("\nCountry:", country[x])
-        print("\nIATA:", codes[x])
-        print("\nTimezone:", timezone[x])
-        print("\nLatitude:", lat[x])
-        print("\nLongitude:", long[x])
+    print("Airports in", city,":\n")
+    for i, j, k, l, m, n in zip(names, country, codes, timezone, lat, long):
+        print("Aiport Name: ", i)
+        print("Country: ", j)
+        print("Code: ", k)
+        print("Latitude:", m, "Longitude:", n, "\n")        
+     
+    
+#Main menu function
+
+#if statement to control main menu based on our sample projects, so far this only contains sections 
+#from airline/airport facts from option variable above. 
+#option 4 shows airport facts
+def main_menu1():
+    option = input("Welcome to the Airline Search Engine! Plese Select an Option From Below: \n 1) Airline/Airport Information \n 2) Plan a Trip \n 3) FInd an Airport \n 4) Exit\n")
+    if option == '1':
+        option1 = input("Please Select From Option Below \n 1) View Airports in Your Country \n 2) Countries with Highest Number of Airports \n 3) Cities with Most Airline Traffic \n")
+        if option1 == '1':
+            airports_by_country()
+            go_back()
+        if option1 == '2':
+            country_rankings()
+            go_back()
+        if option1 == '3':
+            find_popular_airport()
+            go_back()
         
         
-       
-################################################### Country and airport code#######################
-from pyspark.sql import SQLContext
-from pyspark import SparkFiles
-from pyspark import SparkContext
-sc =SparkContext()
-url = "https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat"
-sc.addFile(url)
-sqlContext = SQLContext(sc)
-df = sqlContext.read.csv(SparkFiles.get("airports.dat"), header=False, inferSchema= True) 
+    if option == '2':
+        find_flight()
+        go_back()
+        
+    if option == '3':
+        airport_info()
+        go_back()
 
-new_col_names = ['ID', 'Name', 'City', 'Country','IATA', 'ICAO', 'Lat', 'Long', 'Alt', 'Timezone', 'DST', 'Tz database time zone', 'type', 'source']
-df = df.toDF(*new_col_names)
-
-from pyspark.sql.functions import col
-import pyspark.sql.functions as F
-from pyspark.sql import SparkSession
-
-def airports_by_country():
-    area = input("Please enter a country:")
+# Option == 4 means exit the program 
+    if option == '4':
+        print("Goodbye")
+        
+        
+def run_distributed():
+    main_menu1()
     
-    country_of_area = df.filter(col("Country") == area)
-    data_output = list(country_of_area.select('Name').toPandas()['Name'])
-    data_output1 = list(country_of_area.select('City').toPandas()['City'])
     
-    print("The following airports are in", area, ":")
-    print(data_output)
-    print("\n")
-    print("The following airports in", area, " are in their respective city", ":")
-    print(data_output1)
-    
-airports_by_country()
+run_sequential()
