9208cd2ad5e7178e1b6102478094ac3af0ecadbc
greg-pedersen <71673441+greg-pedersen@users.noreply.github.com>
2020-12-10 15:33:49 -0800

    Update sequential.py
    Final product/code for our sequential function

--- sequential.py
+++ sequential.py
@@ -19,13 +19,16 @@ routes_df = pd.read_csv("https://raw.githubusercontent.com/jpatokal/openflights/
 airline_df = pd.read_csv("https://raw.githubusercontent.com/jpatokal/openflights/master/data/airlines.dat",
                          names = ["Airline ID", "Airline Name", "Alias", "IATA", "ICAO", "Callsign", "Country", "Active (Y/N)"])
 
-#Setting up the main menu
 
-option = input("Welcome to the Airline Search Engine! Plese Select an Option From Below: \n 1) Airline/Airport Facts \n 2) Plan a Trip \n 3) Exit\n")
-
-
-########################################## Airline Statistics Functions #############################################    
-
+#Function for going back in menu:
+def go_back():
+    print("\n\n")
+    input1 = input("Please press enter to go back to main menu")
+    if input1 == "":
+        main_menu()
+        
+        
+#Airports by country
 
 ## Function that asks for a country and outputs all the airports in the country, with corresponding location city
 def find_airports_by_country():
@@ -34,12 +37,11 @@ def find_airports_by_country():
     for index, row in airport_df.iterrows():       
         if row["Country"] == area:
             print(row["Name"], "in", row["City"])
-       
+            
+            
+#Find popular airports
 
-##################################################################find_airline_traffic alternate##################################################################
 #Accepts a ranking parameter and ranks the most popular airports by name and flights in + flights out
-
-
 def get_popular_airports():
     
     amount = input("How many airports would you like to see ranked: ")
@@ -60,16 +62,21 @@ def get_popular_airports():
             
     for item, element in zip(new_list, traffic):
         print(item, ":", element[1])
+        
+        
+#Find an airport 
 
 # Function that asks for an airport and gives out info about that airport
 def find_an_airport():
-    airport = input("Please type in an airport name: ")
-    airport = airport.lower() + " airport"
-    for index, row in airport_df.iterrows():
-        if row["Name"].lower() == airport:
-            print(row["Name"],"\nLocation:",row["City"],",", row["Country"],"\nIATA:",row["IATA"],"\nLatitude:",row["Lat"],"\nLongitude:",row["Long"],"\nTimezone:",row["Timezone"])
-            
-            
+    city = input("Please enter a city: ")
+    newdf = airport_df['City'] == city
+    filtered = airport_df[newdf]
+    for index, row in filtered.iterrows():
+        print(" Airport Name:",row['Name'],"\n","Country:", row["Country"], "\n","Code:", row["IATA"], "\n","Latitude:", row["Lat"], "Longitude", row["Long"], "\n" )
+        
+        
+#Find a flight fro one city to another
+
 #Function that finds flights based on departure and arrival city inputs by user. Outputs Departure airport, to arrival airport, on airlines, with X stops.             
 def find_flights():
     #Accepting Input
@@ -113,30 +120,50 @@ def find_flights():
                 dest1.append(row["Name"])
     for item1, item2, item3, item4, in zip(source1, dest1, names, stops):
         print("Flight from", item1, "to", item2, "on", item3, "with", item4, "stops")
-
+        
+#Main menu function
 
 
 #if statement to control main menu based on our sample projects, so far this only contains sections 
 #from airline/airport facts from option variable above. The only thing needed from section airline/airport facts 
 #is city with most airline traffic (option1 == 3)
 #option 4 shows airport facts
-if option == '1':
-    option1 = input("Please Select From Option Below \n 1) View Airports in Your Country/City \n 2) Countries with Highest Number of Airports \n 3) Cities with Most Airline Traffic \n 4) Airport Facts\n")
-    if option1 == '1':
-        find_airports_by_country()
-    if option1 == '2':
-        ranking = input("How many cities would you like to see ranked? ")
-        frequency = airport_df["Country"].value_counts()
-        print(frequency[0:int(ranking)])
-    if option1 == '3':
-        get_popular_airports()
-    if option1 == '4':
-        find_an_airport()
+def main_menu():
+    option = input("Welcome to the Airline Search Engine! Plese Select an Option From Below: \n 1) Airline/Airport Information \n 2) Find Flights \n 3) Find an Airport \n 4) Exit\n")
+    if option == '1':
+        option1 = input("Please Select From Option Below \n 1) View Airports in Your Country \n 2) Countries with Highest Number of Airports \n 3) Cities with Most Airline Traffic \n")
+        if option1 == '1':
+            find_airports_by_country()
+            go_back()
+        if option1 == '2':
+            ranking = input("How many cities would you like to see ranked? ")
+            frequency = airport_df["Country"].value_counts()
+            print(frequency[0:int(ranking)])
+            go_back()
+        if option1 == '3':
+            get_popular_airports()
+            go_back()
         
 # Still needing an option == 2, which would be the trip recommendations section, coming soon. 
-if option == '2':
-    find_flights()
+    if option == '2':
+        find_flights()
+        go_back()
+        
+    if option == '3':
+        find_an_airport()
+        go_back()
+    
+    if option == '4':
+        print("Goodbye")
 
-# Option == 3 means exit the program 
-if option == '3':
-    print("Goodbye")
+
+# Option == 4 means exit the program 
+   
+  
+ 
+#Run Sequential function:
+
+def run_sequential():
+    main_menu()
+    
+run_sequential()
